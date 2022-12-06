eda6e232fa713cf162292991149dd6e6d7308b01
SahilMahadwar <accounts@mahadwar.com>
2022-12-07 00:44:34 +0530

    🔥 Removed trpc example boilerplate

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,11 +3,13 @@ import { type NextPage } from "next";
 import { signIn, useSession } from "next-auth/react";
 
 const Home: NextPage = () => {
-  const { data: session } = useSession();
+  const { data: session, status } = useSession();
   return (
     <PageLayout>
       <div>Is this inter</div>
       <p>{JSON.stringify(session?.user)}</p>
+      <p>{status}</p>
+
       <button
         className="rounded-md bg-red-500 px-4 py-2 text-white"
         onClick={() => signIn()}
