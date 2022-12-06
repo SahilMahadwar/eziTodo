a09653b36667f80f5273f54a8f8ffe072edc08e3
SahilMahadwar <accounts@mahadwar.com>
2022-12-07 00:30:33 +0530

    ✅ Nextauth discord provider

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,19 @@
 import PageLayout from "@ui/PageLayout";
 import { type NextPage } from "next";
+import { signIn, useSession } from "next-auth/react";
 
 const Home: NextPage = () => {
+  const { data: session } = useSession();
   return (
     <PageLayout>
       <div>Is this inter</div>
+      <p>{JSON.stringify(session?.user)}</p>
+      <button
+        className="rounded-md bg-red-500 px-4 py-2 text-white"
+        onClick={() => signIn()}
+      >
+        Login
+      </button>
     </PageLayout>
   );
 };
